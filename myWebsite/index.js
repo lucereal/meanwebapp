d662c05ecfcb61db39d57239375d0c970bf537af
David Lucero <david.lucero@ttu.edu>
2018-06-12 21:15:01 -0500

    commit

--- myWebsite/index.js
+++ myWebsite/index.js
@@ -9,6 +9,13 @@ app.get('/', function(req,res){
 	res.render('index');
 });
 
+app.get('/resume', function(req,res){
+	res.send('Resume will go here');
+})
+
+app.get('/projects', function(req,res){
+	res.send('Projects');
+})
 app.listen(3000,function(){
 	console.log('Listening on port 3000.');
 })
